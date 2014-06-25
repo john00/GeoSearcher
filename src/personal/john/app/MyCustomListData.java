fce89d99fc49adbf427941d98ed0e6373fa7c22d
john <pcelsius000@gmail.com>
2014-06-26 00:38:32 +0900

    機能マージ
    マスターのコミット

---
+++ src/personal/john/app/MyCustomListData.java
@@ -0,0 +1,59 @@
+
+package personal.john.app;
+
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+
+public class MyCustomListData {
+    private Bitmap mHotelImg = null;
+
+    private String mHotelName = null;
+
+    private String mHotelInfo = null;
+
+    private String mHotelDistance = null;
+    
+    private String mHotelMinCharge = null;
+
+    public void setHotelImage(Bitmap result) {
+        mHotelImg = result;
+    }
+
+    public Bitmap getHotelImage() {
+        return mHotelImg;
+    }
+
+    public void setHotelName(String name) {
+        mHotelName = name;
+    }
+
+    public String getHotelName() {
+        return mHotelName;
+    }
+
+    public void setHotelInfo(String info) {
+        mHotelInfo = info;
+    }
+
+    public String getHotelInfo() {
+        return mHotelInfo;
+    }
+
+    public void setHotelDistance(String distance) {
+        mHotelDistance = distance;
+    }
+
+    public String getHotelDistance() {
+        return mHotelDistance;
+    }
+
+    public void setHotelMinCharge(String hotelMinCharge) {
+        mHotelMinCharge = hotelMinCharge;
+    }
+    
+    public String getHotelMinCharge() {
+        return mHotelMinCharge;
+    }
+
+
+}
